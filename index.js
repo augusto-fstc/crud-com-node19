b5f1aefc1f32b8c2a363b3626126bb5f38d032a7
fbziin05 <fss29@aluno.ifal.edu.br>
2023-09-17 17:39:16 -0300

    atualizado

--- index.js
+++ index.js
@@ -8,9 +8,9 @@
         console.log(resultado);
 
         const resultadoCreate = await Filme.create({
-            nome: 'Eu sou demais',
-            preco: 10,
-            descricao: 'respeita o papai'
+            nome: 'Curtindo a Vida Adoidado',
+            preco: 150,
+            descricao: 'Um dos filmes bons antigos! No último semestre do curso do colégio, Ferris Bueller sente um incontrolável desejo de matar a aula e planeja um grande programa na cidade com sua namorada, seu melhor amigo e uma Ferrari.'
         })
 
         // Read, depois do findALL basta usar algum número para arrumar a posição do array ou objeto, tanto faz como queira chamar.
@@ -27,9 +27,9 @@
         // UPDATE
 
         //DELETE
-        //Filme.destroy({ where: { id: 1 }});
+        // Filme.destroy({ where: { id: 0 }});
 
-        //const filme = await filme.findByPk(1);
+        //const filme = await filme.findByPk(0);
         //filme.destroy();
         //DELETE
 
